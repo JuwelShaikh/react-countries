ee643cf1c77a0948a2468f94ce173876555d76df
JuwelShaikh <juwelshaikh04@gmail.com>
2023-12-16 15:23:20 +0600

    state declear

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [] = useState([]);
   return (
     <div className="App">
       <loadCountries></loadCountries>
