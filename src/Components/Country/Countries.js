504c9ca560edb818c5bd6f125349dc7d36ea7028
JuwelShaikh <juwelshaikh04@gmail.com>
2023-12-17 14:49:45 +0600

    aefwrf

--- src/Components/Country/Countries.js
+++ src/Components/Country/Countries.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Countries = () => {
+    const [country, setCountry] = useState([])
+    useEffect(()=>{
+        fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(data => setCountry(data))
+    },[])
+
     return (
         <div>
-            
+            <h1>All Countries info</h1>
+            <h5>Country Available: {country.length}</h5>
         </div>
     );
 };
