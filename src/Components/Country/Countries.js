d3fbd3bdfc0529fde292c469e4effade02f613f5
JuwelShaikh <juwelshaikh04@gmail.com>
2023-12-17 14:56:23 +0600

    Data adding

--- src/Components/Country/Countries.js
+++ src/Components/Country/Countries.js
@@ -12,8 +12,20 @@ const Countries = () => {
         <div>
             <h1>All Countries info</h1>
             <h5>Country Available: {country.length}</h5>
+            {
+                country.map(country => <InfoCountry name={country.name.common} capital={country.capital}></InfoCountry>)
+            }
         </div>
     );
 };
+
+const InfoCountry = props => {
+    return(
+        <div style={{backgroundColor:'Tomato', width:'max-content', padding:'2px', margin:'5px'}}>
+            <h3>Name: {props.name}</h3>
+            <h4>Capital: {props.capital}</h4>
+        </div>
+    )
+} 
 
 export default Countries;
