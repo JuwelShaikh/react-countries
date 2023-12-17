03e6dac1fb77f2c19a16c67f5d78730f60ea2340
JuwelShaikh <juwelshaikh04@gmail.com>
2023-12-17 14:16:14 +0600

    Adding new compo files

--- src/App.js
+++ src/App.js
@@ -5,39 +5,39 @@ import { useEffect, useState } from 'react';
 function App() {
   return (
     <div className="App">
-      <LoadCountries></LoadCountries>
+      
     </div>
   );
 }
 
-function LoadCountries(){
-  const [countries, setCountries] = useState([]);
+// function LoadCountries(){
+//   const [countries, setCountries] = useState([]);
 
-  useEffect(()=>{
-    fetch('https://restcountries.com/v3.1/all')
-    .then(res => res.json())
-    .then(data => setCountries(data));
+//   useEffect(()=>{
+//     fetch('https://restcountries.com/v3.1/all')
+//     .then(res => res.json())
+//     .then(data => setCountries(data));
     
-  },[])
-  return(
-    <div>
-      <h1>All countries!!</h1>
-      <h3>Available countries:{countries.length}</h3>
-      {
-        countries.map(country => <Country name={country.name.common} population={country.population} capital={country.capital} ></Country>)
-      }
-    </div>
-  )
-}
+//   },[])
+//   return(
+//     <div>
+//       <h1>All countries!!</h1>
+//       <h3>Available countries:{countries.length}</h3>
+//       {
+//         countries.map(country => <Country name={country.name.common} population={country.population} capital={country.capital} ></Country>)
+//       }
+//     </div>
+//   )
+// }
 
-function Country(props){
-  return(
-    <div className='count'>
-      <h2>Name:{props.name}</h2>
-      <h3>Capital:{props.capital}</h3>
-      <p>Population:{props.population}</p>
-    </div>
-  )
-}
+// function Country(props){
+//   return(
+//     <div className='count'>
+//       <h2>Name:{props.name}</h2>
+//       <h3>Capital:{props.capital}</h3>
+//       <p>Population:{props.population}</p>
+//     </div>
+//   )
+// }
 
 export default App;
